9106f70de6ce726838e9353edbd292d18c2c1763
filipolszewski <filipolszew@o2.pl>
2018-01-26 00:00:17 +0100

    working v1, only for my img size

--- EditorController.py
+++ EditorController.py
@@ -1,28 +1,43 @@
-from PIL import Image
-import copy
+from PIL import Image, ImageEnhance, ImageDraw
+
 
 class EditorController:
     def __init__(self, cont_size):
         self.cont_width = cont_size.width()
         self.cont_height = cont_size.height()
+        # working = image we work on. Apply btn saves current one to working
         self.working_image = None
         self.current_image = None
         self.file_path = None
+        self.monolith_checked = False
+
+    def save_final_img(self):
+        save_path = "output/" + self.file_path.split("/")[-1]
+        if self.monolith_checked:
+            self.draw_monolith(self.current_image).save(save_path)
+        else:
+            self.current_image.save(save_path)
 
     def set_working_image(self, filename):
         self.file_path = filename
+
         # TODO Parametrize which part of image to cut(?)
         img = Image.open(filename).convert("RGBA")
         # expected img size at this point - 4592x3072
         # 3072 -> 2583
         crop_rectangle = (0, 244, 4592, 2828)
         cropped_img = img.crop(crop_rectangle)
-        self.working_image = cropped_img
+
+        self.working_image = self.black_and_white(cropped_img)
         self.current_image = self.working_image.copy()
         print(self.current_image)
 
     def get_current_image(self):
-        return self._resize_to_cont(self.current_image)
+        if self.current_image is None:
+            return None
+        if self.monolith_checked:
+            return self._resize_to_cont(self.draw_monolith(self.current_image.copy()))
+        return self._resize_to_cont(self.current_image.copy())
 
     def _resize_to_cont(self, current_image):
         current_image.thumbnail((self.cont_width, self.cont_height), Image.ANTIALIAS)
@@ -31,4 +46,31 @@ class EditorController:
     def apply_changes(self):
         self.working_image = self.current_image.copy()
 
+    @staticmethod
+    def black_and_white(img):
+        return ImageEnhance.Color(img).enhance(0.0)
 
+    def change_brightness_contrast(self, b_param, c_param):
+        b_factor = ((b_param / 123) + 1) * 1.2
+        c_factor = ((c_param / 123) + 1) * 1.2
+        tmp_img = self.working_image.copy()
+        enhancer_b = ImageEnhance.Brightness(tmp_img)
+        tmp_img = enhancer_b.enhance(b_factor)
+        enhancer_c = ImageEnhance.Contrast(tmp_img)
+        self.current_image = enhancer_c.enhance(c_factor)
+
+    def monolith_change(self, checked):
+        self.monolith_checked = checked
+
+    def draw_monolith(self, img):
+        a = (2000, 450)
+        b = (2596,  2583 - a[1])
+        draw = ImageDraw.Draw(img)
+        draw.rectangle((a, b), fill="black")
+        return img
+
+    def clear(self):
+        self.working_image = None
+        self.current_image = None
+        self.file_path = None
+        self.monolith_checked = False
--- EditorMain.py
+++ EditorMain.py
@@ -13,36 +13,60 @@ class MyForm(QWidget):
         self.ui = Ui_EditorForm()
         self.ui.setupUi(self)
         self._create_handlers(self.ui)
-        self.current_filename = None
+
         self.image_controller = EditorController(self.ui.image_container.frameGeometry())
+        # these 2 lines just for testing!
+        self.image_controller.set_working_image("input/DSC_0429.JPG")
+        self.refresh_image_container()
 
     def _create_handlers(self, ui):
         ui.load_image_btn.clicked.connect(self.load_btn_click)
         ui.save_image_btn.clicked.connect(self.save_btn_click)
         ui.apply_btn.clicked.connect(self.apply_btn_click)
+        ui.brightness_slider.sliderReleased.connect(self.b_or_c_changed)
+        ui.contrast_slider.sliderReleased.connect(self.b_or_c_changed)
+        ui.monolith_cbox.stateChanged.connect(self.monolith_clicked)
 
     def load_btn_click(self):
-
         filters = "Images (*.png *.jpg)"
         filename, _ = QFileDialog.getOpenFileName(None, "Wybierz zdjęcie", "input/", filters)
         if filename is not None:
+            self.clear()
             self.current_filename = filename
             self.image_controller.set_working_image(self.current_filename)
             self.refresh_image_container()
 
     def save_btn_click(self):
-        pass
+        self.image_controller.save_final_img()
 
     def apply_btn_click(self):
         self.image_controller.apply_changes()
         self.ui.brightness_slider.setValue(0)
         self.ui.contrast_slider.setValue(0)
 
+    def b_or_c_changed(self):
+        self.image_controller.change_brightness_contrast(self.ui.brightness_slider.value(),
+                                                         self.ui.contrast_slider.value())
+        self.refresh_image_container()
+
+    def monolith_clicked(self):
+        self.image_controller.monolith_change(self.ui.monolith_cbox.isChecked())
+        self.refresh_image_container()
+
     def refresh_image_container(self):
         current_image = self.image_controller.get_current_image()
         if current_image:
             pixmap = QPixmap.fromImage(ImageQt(current_image))
             self.ui.image_container.setPixmap(pixmap)
+        else:
+            self.ui.image_container.clear()
+
+    def clear(self):
+        self.image_controller.clear()
+        self.ui.contrast_slider.setValue(0)
+        self.ui.brightness_slider.setValue(0)
+        self.ui.monolith_cbox.setChecked(False)
+        self.refresh_image_container()
 
 
 if __name__ == "__main__":
